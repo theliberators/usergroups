99ec46314c5da6a5474673ab47fc181d21732b8e
Benj Huser <benj.huser@gmail.com>
2021-06-05 15:01:59 +0200

    Features/illustration page overview (#1)
    * Create script to generate html page with all illustrations
    
    * Add github action workflow
    
    * Execute python script in workflow
    
    * Use forward slashes
    
    * Add path filter to workflow
    
    * Update python script description
    
    * Adjust include path
    
    * Only push new pages if we're on main branch
    
    * Don't run action if we're not in main branch
    
    Co-authored-by: Benj Huser <benjamin.huser@hitachi-powergrids.com>

---
+++ Assets/Illustrations/create_illustration_page/create_illustration_page.py
@@ -0,0 +1,47 @@
+import glob
+from dominate import document
+from dominate.tags import *
+import sys
+import os
+
+# Script that creates an html file that displays all images of a specified folder.
+
+# Styles could be supplied via styles.css file.
+# -----------------------
+# prerequisite: dominate
+# pip install dominate OR pip install --no-cache-dir -r requirements.txt
+# -----------------------
+
+# Inputs:
+# 1: input directory (folder containing images)
+# 2: document title (header displayed on top)
+# Output:
+# index.html will be created next to the images - Keep file in there as paths in index.html are relative
+
+scriptDirectory = os.path.dirname(os.path.realpath(__file__))
+inputDirectory = os.path.join(scriptDirectory, "../")
+documentTitle = "The Liberators Illustrations"
+
+if len(sys.argv) == 4:
+    print('Using custom input directory and document title')
+    inputDirectory = sys.argv[1]
+    documentTitle = sys.argv[3]
+
+outputFile = os.path.join(inputDirectory, "index.html")
+print('Input Directory: ', inputDirectory)
+print('Output File: ', outputFile)
+print('Document Title: ', documentTitle)
+
+images = glob.glob(inputDirectory + '/*.png')
+
+with document(title=documentTitle) as doc:
+    h1(documentTitle)
+    for path in images:
+        span(img(src=os.path.basename(path), style="width:30%"), _class='photo')
+
+with doc.head:
+    link(rel='stylesheet', href='style.css')
+
+
+with open(outputFile, 'w') as f:
+    f.write(doc.render())
